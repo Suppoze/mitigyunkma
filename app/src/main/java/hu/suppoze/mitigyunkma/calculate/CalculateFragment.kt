0bc6b616dd49552bad3dd8caef39130644b987b7
Tegyukfel <tegyukfel@gmail.com>
2015-12-26 00:56:01 +0100

    [ Calculate ] Basic numpad [ Git ] Git flow init

--- app/src/main/java/hu/suppoze/mitigyunkma/calculate/CalculateFragment.kt
+++ app/src/main/java/hu/suppoze/mitigyunkma/calculate/CalculateFragment.kt
@@ -1,6 +1,5 @@
 package hu.suppoze.mitigyunkma.calculate
 
-import android.animation.ObjectAnimator
 import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
 import android.support.design.widget.TextInputLayout
@@ -16,7 +15,6 @@ import butterknife.OnClick
 import hu.suppoze.mitigyunkma.R
 import org.jetbrains.anko.support.v4.alert
 
-
 class CalculateFragment : Fragment() {
 
     @Bind(R.id.calculate_view_capacity) lateinit var capacityField: TextInputLayout
@@ -27,6 +25,20 @@ class CalculateFragment : Fragment() {
     @Bind(R.id.button_save_background) lateinit var saveButtonBackground: RelativeLayout
     @Bind(R.id.button_reset) lateinit var resetButton: RelativeLayout
 
+    // TODO: Create custom numpad view
+    @Bind(R.id.numpad_0) lateinit var num0: Button
+    @Bind(R.id.numpad_1) lateinit var num1: Button
+    @Bind(R.id.numpad_2) lateinit var num2: Button
+    @Bind(R.id.numpad_3) lateinit var num3: Button
+    @Bind(R.id.numpad_4) lateinit var num4: Button
+    @Bind(R.id.numpad_5) lateinit var num5: Button
+    @Bind(R.id.numpad_6) lateinit var num6: Button
+    @Bind(R.id.numpad_7) lateinit var num7: Button
+    @Bind(R.id.numpad_8) lateinit var num8: Button
+    @Bind(R.id.numpad_9) lateinit var num9: Button
+    @Bind(R.id.numpad_delete) lateinit var numDelete: Button
+    @Bind(R.id.numpad_decimal) lateinit var numDecimal: Button
+
     val presenter: CalculatePresenter = CalculatePresenter(this)
 
     override fun onCreateView(inflater: LayoutInflater?,
@@ -36,12 +48,53 @@ class CalculateFragment : Fragment() {
         ButterKnife.bind(this, view)
 
         percentageField.editText.addTextChangedListener(presenter)
+        percentageField.editText.showSoftInputOnFocus = false
+
         capacityField.editText.addTextChangedListener(presenter)
+        capacityField.editText.showSoftInputOnFocus = false
+
         priceField.editText.addTextChangedListener(presenter)
+        priceField.editText.showSoftInputOnFocus = false
+
+        initializeNumpad()
 
         setSaveButtonEnabled(false)
 
         return view
+    }
+
+    private fun initializeNumpad() {
+
+        // TODO: Create custom numpad view
+        num0.setOnClickListener { writeInFocused("0") }
+        num1.setOnClickListener { writeInFocused("1") }
+        num2.setOnClickListener { writeInFocused("2") }
+        num3.setOnClickListener { writeInFocused("3") }
+        num4.setOnClickListener { writeInFocused("4") }
+        num5.setOnClickListener { writeInFocused("5") }
+        num6.setOnClickListener { writeInFocused("6") }
+        num7.setOnClickListener { writeInFocused("7") }
+        num8.setOnClickListener { writeInFocused("8") }
+        num9.setOnClickListener { writeInFocused("9") }
+        numDecimal.setOnClickListener { writeInFocused(".") }
+        numDelete.setOnClickListener { deleteFromFocused() }
+    }
+
+    private fun deleteFromFocused() {
+        val focusedTextField = determineFocused()?.text
+        if (focusedTextField.isNullOrEmpty()) return
+        focusedTextField?.delete(focusedTextField.length - 1, focusedTextField.length)
+    }
+
+    private fun writeInFocused(s: String) {
+        determineFocused()?.text?.append(s)
+    }
+
+    private fun determineFocused(): EditText? {
+        if (capacityField.editText.isFocused) return capacityField.editText
+        else if (percentageField.editText.isFocused) return percentageField.editText
+        else if (priceField.editText.isFocused) return priceField.editText
+        throw RuntimeException("CalculateFragment: Determining focus failed.")
     }
 
     override fun onResume() {
@@ -85,5 +138,4 @@ class CalculateFragment : Fragment() {
             positiveButton("Cool, brah") {}
         }.show()
     }
-
 }
--- app/src/main/java/hu/suppoze/mitigyunkma/calculate/CalculatePresenter.kt
+++ app/src/main/java/hu/suppoze/mitigyunkma/calculate/CalculatePresenter.kt
@@ -3,6 +3,7 @@ package hu.suppoze.mitigyunkma.calculate
 import android.support.v4.app.Fragment
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import hu.suppoze.mitigyunkma.core.IndexCalculator
 import org.jetbrains.anko.async
 import org.jetbrains.anko.uiThread
@@ -19,33 +20,39 @@ class CalculatePresenter(fragment: Fragment) : TextWatcher {
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {    }
 
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        if (noEmptyFields()) {
-            async {
+        async() {
 
+            if (noEmptyFields() && zeroCheck()) {
                 val index = calculate()
 
                 uiThread {
                     view.drinkIndex.text = index.toString()
                     view.setSaveButtonEnabled(true)
                 }
+            } else {
+                uiThread {
+                    view.setSaveButtonEnabled(false)
+                    view.drinkIndex.text = ""
+                }
             }
-        } else {
-            view.setSaveButtonEnabled(false)
         }
     }
 
-    private fun noEmptyFields(): Boolean {
-        return view.capacityField.editText.text.isNotEmpty() &&
-                view.percentageField.editText.text.isNotEmpty() &&
-                view.priceField.editText.text.isNotEmpty()
-    }
+    private fun noEmptyFields(): Boolean =
+        view.capacityField.editText.text.isNotEmpty() &&
+        view.percentageField.editText.text.isNotEmpty() &&
+        view.priceField.editText.text.isNotEmpty()
 
-    private fun calculate(): Int {
-        return IndexCalculator.calculateIndex(
-                    IndexCalculator.ParameterBundle(
-                            percent = view.percentageField.editText.text.toString(),
-                            capacity = view.capacityField.editText.text.toString(),
-                            price = view.priceField.editText.text.toString()))
-    }
+    private fun zeroCheck(): Boolean =
+        view.capacityField.editText.text.toString().toDouble() > 0 &&
+        view.percentageField.editText.text.toString().toDouble() > 0 &&
+        view.priceField.editText.text.toString().toInt() > 0
+
+    private fun calculate(): Int =
+        IndexCalculator.calculateIndex(
+                IndexCalculator.ParameterBundle(
+                        percent = view.percentageField.editText.text.toString(),
+                        capacity = view.capacityField.editText.text.toString(),
+                        price = view.priceField.editText.text.toString()))
 
 }
