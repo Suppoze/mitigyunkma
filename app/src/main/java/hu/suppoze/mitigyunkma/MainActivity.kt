10905a261417d940fc0fce176090c5672f19b12f
Tegyukfel <tegyukfel@gmail.com>
2016-08-01 20:38:38 +0200

    Changed colors of appbarlayout.

--- app/src/main/java/hu/suppoze/mitigyunkma/MainActivity.kt
+++ app/src/main/java/hu/suppoze/mitigyunkma/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.AppBarLayout
 import android.support.design.widget.CoordinatorLayout
+import android.support.v4.content.ContextCompat
 import android.support.v4.view.ViewPager
 import kotlinx.android.synthetic.main.activity_main.*
 
